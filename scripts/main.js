1243e99d74e90e07c721069ba4b86e5cd814bb79
cychoo61 <cychoo61@gmail.com>
2022-11-16 19:15:00 +0900

    change again

--- scripts/main.js
+++ scripts/main.js
@@ -9,21 +9,23 @@ myImage.onclick = function() {
     }
 }
 let myButton = document.querySelector('button');
-//let myHeading = document.querySelector('h1');
+let myHeading = document.querySelector('h1');
 function setUserName() {
   let myName = prompt('Please enter your name.');
   if(!myName || myName === null) {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    //myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    myHeading.innerHTML = 'h1, ' + myName;
   }
 }
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  //myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  myHeading.textContent = 'h1, ' + storedName;
 }
 myButton.onclick = function() {
   setUserName();
